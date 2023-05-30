c9c60f3ba082a3e5d923b9f05561f6fbdb3c26ac
Udav-94 <ups-fti@yandex.ru>
2023-05-30 20:44:59 +0300

    Реализована первая задачы в д.з. про функции создана функция getArrayParams

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,26 @@
 function getArrayParams(...arr) {
+  let min=arr[0];
+  let max=arr[0];
+  let avg=arr[0];
+
+  for (let i=1; i<arr.length; i++) {
+    if (arr[i] < min) {
+      min=arr[i];
+    }
+  }
+
+  for (let i=1; i<arr.length; i++) {
+    if (arr[i] > max) {
+      max=arr[i];
+    }
+  }
+
+  for (let i=1; i<arr.length; i++) {
+    avg = avg + arr[i];
+    }
+
+    avg = avg / arr.length * 100;
+    avg = Math.round(avg) / 100;
 
   return { min: min, max: max, avg: avg };
 }
