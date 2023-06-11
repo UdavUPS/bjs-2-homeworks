8bfaa74dd4137f738fb5f5e99ba7999289a1593b
Udav-94 <ups-fti@yandex.ru>
2023-06-11 16:48:39 +0300

    Добавлено корректно работающее решение к заданию лекции №4_Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let studentVasya = new Student('Вася', 'male', 20);
+let studentVera = new Student('Вера', 'female', 19);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if ('marks' in this) {
+    for (let i of marks) {
+        this.marks.push(i);
+    }
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ('marks' in this && this.marks.length != -1) {
+        return this.marks.reduce((acc, item)=> acc + item / this.marks.length, 0);
+      } else {
+        return 0;
+      }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
